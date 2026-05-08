08f435fb868f564dbc3f12a905dbc69d4fdbd021
mahery069 <otisoami@gmail.com>
2026-05-08 08:05:32 +0300

    base config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'projet_regime_alimentaire',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
